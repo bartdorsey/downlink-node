02afeadaed6381bbdcbb58684da81a49a11efc45
Bart Dorsey <bart@bartdorsey.com>
2021-02-15 11:09:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const fetch = require('node-fetch');
+const wallpaper = require('wallpaper');
+const fs = require('fs').promises;
+
+const DOWNLINK_SOURCES = 'https://downlinkapp.com/sources.json';
+const MAP = 'Continental US';
+const WALLPAPER_FILE = 'wallpaper.jpg';
+
+async function init() {
+  const { sources } = await getDownlinkSources();
+  const map = sources.find(source => {
+    return source.name === MAP;
+  });
+  await getWallpaper(map.url.full);
+  await setWallpaper();
+}
+
+async function getWallpaper(url) {
+  try {
+    const response = await fetch(url);
+  
+    const image = await response.blob();
+    const buffer = await image.arrayBuffer();
+    await fs.writeFile('wallpaper.jpg', Buffer.from(buffer), 'binary');
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+async function setWallpaper() {
+  try {
+    await wallpaper.set('wallpaper.jpg');
+  } 
+  catch (e) {
+    console.error(e);
+  }
+}
+
+async function getDownlinkSources() {
+  try {
+    const response = await fetch(DOWNLINK_SOURCES);
+    if (!response.ok) {
+      throw response;
+    }
+    return await response.json();
+  }
+  catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+init();
